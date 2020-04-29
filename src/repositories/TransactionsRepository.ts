f1e126eeed374fb672527716cc22a921daaad4fc
Higo Ribeiro <HORBJN@gmail.com>
2020-04-29 20:40:10 -0300

    chore: configure the transaction repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance: Balance = transactions.reduce(
+      (prev, transaction) => {
+        prev[transaction.type] += transaction.value;
+        prev.total = prev.income - prev.outcome;
+
+        return prev;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      } as Balance,
+    );
+
+    return balance;
   }
 }
 
